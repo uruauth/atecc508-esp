c1e9ff9bfbf210fd874ad958818cf4aaa2c4db1b
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2020-03-01 15:40:02 +0100

    .

--- include/atecc508a_conf.h
+++ include/atecc508a_conf.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include <esp_err.h>
+
 esp_err_t atecc508a_read_config_zone();
--- include/atecc508a_util.h
+++ include/atecc508a_util.h
@@ -2,17 +2,7 @@
 
 #include <esp_err.h>
 
-#ifndef ESP_CHECK_RET
-#define ESP_CHECK_RET(f)                                                                                    \
-    {                                                                                                       \
-        esp_err_t _ret = (f);                                                                               \
-        if (_ret != ESP_OK)                                                                                 \
-        {                                                                                                   \
-            ESP_LOGE(LOG_TAG, "Error %s (0x%X) at %s:%d", esp_err_to_name(_ret), _ret, __FILE__, __LINE__); \
-            return _ret;                                                                                    \
-        }                                                                                                   \
-    }
-#endif
+#include "uru-lib.h"
 
 /**
  * @brief
